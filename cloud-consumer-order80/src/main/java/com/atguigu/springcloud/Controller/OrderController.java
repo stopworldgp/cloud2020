b3ebe3350166f5a20c79676246658db71970e79c
stopworldgp <guopeng_huang@163.com>
2020-07-30 20:39:28 +0800

    【新增】提交工程

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/Controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/Controller/OrderController.java
@@ -57,5 +57,12 @@ public class OrderController {
         URI uri = serviceInstance.getUri();
         return restTemplate.getForObject(uri + "/payment/get/1", String.class);
     }
+    // ====================> zipkin+sleuth
+    @GetMapping("/consumer/payment/zipkin")
+    public String paymentZipkin()
+    {
+        String result = restTemplate.getForObject("http://localhost:8001"+"/payment/zipkin/", String.class);
+        return result;
+    }
 
 }
---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/StreamMqMain8802.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author theone
+ * @Date 2020/7/16 21:36
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class StreamMqMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMqMain8802.class, args);
+    }
+}
